7a7f69ed8931bc4f1e46f8ddddab41476171e4db
buggsboony <boonydeboon@gmail.com>
2020-07-11 16:21:39 -0400

    Rajout de install et uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#install stuff
+what=scan
+extension=.php
+#peut être extension vide 
+ 
+echo "killing running instances"
+killall $what
+
+echo "Supprimer lien symbolique vers usr bin"
+sudo rm /usr/bin/$what
+
+echo "done."
+
